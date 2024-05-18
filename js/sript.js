16b8219ac3116f5bbf022b2f2efd081381c7b9c1
Cruz200 <69443073+Cruz200@users.noreply.github.com>
2024-05-18 20:40:50 -0300

    Add files via upload

---
+++ js/sript.js
@@ -0,0 +1,27 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.primerCarrusel img');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.primerCarrusel').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Auto-slide
+setInterval(() => {
+    nextSlide();
+}, 3000); // Cambia de imagen cada 3 segundos
